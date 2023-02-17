ddffbd51d1fec3ae39a3e75edfba5e2065b3eed0
hannah <yuhan.chi12@gmail.com>
2023-02-17 14:05:40 -0800

    add assignment 7 lab

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
             <div className="container">
                 <Routes>
                     {/*<Route path="/labs/a6"*/}
-                    <Route index element={<Labs/>}/>
-                    <Route path="/hello" element={<HelloWorld/>}/>
+                    <Route path="/*" element={<Labs/>}/>
+                    {/*<Route path="/hello" element={<HelloWorld/>}/>*/}
                     <Route path="/tuiter/*" element={<Tuiter/>}/>
                 </Routes>
             </div>
--- src/labs/index.js
+++ src/labs/index.js
@@ -1,11 +1,20 @@
 import Assignment6 from "./a6";
+import Assignment7 from "./a7";
+import {Routes, Route} from "react-router";
 import Nav from "../nav";
+
 function Labs() {
     return (
         <div>
             <Nav/>
-            <Assignment6/>
+            <Routes>
+                <Route index
+                       element={<Assignment6/>}/>
+                <Route path="a7"
+                       element={<Assignment7/>}/>
+            </Routes>
         </div>
     );
 }
+
 export default Labs;
