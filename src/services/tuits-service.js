acfe07f1925934d556b5d3b77dd81f92adfbd160
hannah <yuhan.chi12@gmail.com>
2023-03-28 19:17:13 -0700

    assignment 9 use mongodb server

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-// const TUITS_API = 'https://tuiter-node-server-app-3jr9.onrender.com/api/tuits';
 const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
 export const createTuit = async (tuit) => {
